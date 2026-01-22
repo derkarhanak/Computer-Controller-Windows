4d3294c9c07f982e311b0c7288e96f1a490be00a
ComputerController User <user@computercontroller>
2026-01-22 03:53:58 +0200

    Add .gitignore and MSIX installer support

--- obj/x64/Debug/net8.0-windows10.0.19041.0/ComputerController.AssemblyInfo.cs
+++ obj/x64/Debug/net8.0-windows10.0.19041.0/ComputerController.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComputerController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e2c6123da112ec7ef7809a9d2ea0c69d8c70dc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComputerController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComputerController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
